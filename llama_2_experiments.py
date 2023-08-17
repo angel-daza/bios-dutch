d49de3387fb5552c3defcc5298460ac75664edf7
Angel Daza <josdaza.a@gmail.com>
2023-08-17 13:55:01 +0200

    llama test

--- llama_2_experiments.py
+++ llama_2_experiments.py
@@ -15,16 +15,18 @@ pipeline = transformers.pipeline(
 
 #prompt = "Give me a list of the Named Entitites (PERSON, LOCATION, ORGANIZATION, TIME, WORK_OF_ART, OCCUPATION) in the following sentence: 'Yumi Matsutoya (松任谷 由実, Matsutōya Yumi, born January 19, 1954), nicknamed Yuming (ユーミン, Yūmin), is a Japanese singer, composer, lyricist and pianist.'\n"
 
-prompt = """
+question = "In what date did Mexico become officially independent?"
+
+base_prompt = f"""
 [INST] «SYS»\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. 
 Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. 
 Please ensure that your responses are socially unbiased and positive in nature.\n\n
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. 
-If you don’t know the answer to a question, please don’t share false information.\n«/SYS»\n\nHi, how are you? [/INST] Good thanks! \n[INST] What is the capital of France? [/INST]
+If you don’t know the answer to a question, please don’t share false information.\n«/SYS»\n\nHi, how are you? [/INST] Good thanks! \n[INST] {question} [/INST]
 """
 
 sequences = pipeline(
-    prompt,
+    base_prompt,
     do_sample=True,
     top_k=10,
     num_return_sequences=1,
